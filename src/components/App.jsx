d45e891277e55c84b8550180cfa49a2b13a5d695
JMartLMU <jmart253@lion.lmu.edu>
2024-12-10 14:40:34 -0800

    Prep for merge

---
+++ src/components/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import { SignIn, SignOut } from "./Auth"
+import { useAuthentication } from "../services/authService"
+import { fetchArticles, createArticle } from "../services/articleService"
+import WritingOverlay from "./WritingOverlay"
+import ArticleOverlay from "./ArticleList"
+import "./css/App.css"
+import QuestBoard from "./QuestBoard"
+
+export default function App() {
+  const [articles, setArticles] = useState([])
+  const [article, setArticle] = useState(null)
+  const user = useAuthentication()
+
+  // This is a trivial app, so just fetch all the articles only when
+  // a user logs in. A real app would do pagination. Note that
+  // "fetchArticles" is what gets the articles from the service and
+  // then "setArticles" writes them into the React state.
+  useEffect(() => {
+    if (user) {
+      fetchArticles().then(setArticles)
+      console.log(articles)
+    }
+  }, [user])
+
+  // Update the "database" *then* update the internal React state. These
+  // two steps are definitely necessary.
+  function addArticle({ title, body, danger, reward }) {
+    createArticle({ title, body, danger, reward }).then((article) => {
+      setArticle(article)
+      setArticles([article, ...articles])
+      console.log(articles)
+    })
+  }
+
+  return (
+    <div className="App">
+      <header>
+      <ArticleOverlay user={user} list={articles} quest={article} setArticle={setArticle}/>
+      <WritingOverlay addArticle={addArticle}/>
+      {!user ? <SignIn /> : <SignOut />}
+      </header>
+
+      {!user ? 
+        <p>Please Sign-In to View Quests</p> : <QuestBoard board={articles}/>}
+
+    </div>
+  )
+}
---
+++ src/components/Article.jsx
@@ -0,0 +1,17 @@
+import Draggable from "react-draggable"
+
+export default function Article({ article }) {
+  return (
+    <Draggable>
+      {!article ? (
+        <p>No article selected</p>
+      ) : (
+        <section>
+          <h2>{article.title}</h2>
+          <p className="date">{`Posted: ${article.date}`}</p>
+          <p className="body">{article.body}</p>
+        </section>
+      )}
+    </Draggable>
+  )
+}
