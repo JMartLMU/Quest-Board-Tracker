0a64d76ff49d3564e0df08477cd82fe824bf4428
JMartLMU <jmart253@lion.lmu.edu>
2024-12-10 14:34:15 -0800

    Draggable Update

--- src/components/QuestBoard.jsx
+++ src/components/QuestBoard.jsx
@@ -1,14 +1,11 @@
 import Article from "./Article";
-import {DraggableCore} from "react-draggable"
 
 export default function QuestBoard({ board }) {
     return (
         <div>
             {board.length > 0 ? (
                 board.map((quest, index) => (
-                    <DraggableCore key={index}>
-                        <Article article={quest}/>
-                    </DraggableCore>
+                    <Article key={index} article={quest}/>
                 ))
             ) : (
                 "Nothing to display"
